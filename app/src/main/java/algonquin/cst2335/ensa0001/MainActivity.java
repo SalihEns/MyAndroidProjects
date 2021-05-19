aa365f78b7c344032a1763e359b538eba57a0b4d
salih <ensa0001@algonquinlive.com>
2021-05-19 15:01:27 -0400

    commit: local history saving work

--- app/src/main/java/algonquin/cst2335/ensa0001/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/ensa0001/MainActivity.java
@@ -3,6 +3,9 @@ package algonquin.cst2335.ensa0001;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button myButton = findViewById(R.id.button);
+
+        TextView mytext =  findViewById(R.id.textview);
+
+        EditText myedit = findViewById(R.id.myedittext);
+
+
     }
 }
