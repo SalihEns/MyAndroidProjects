3c99de44710af9e35b58f17b93646588cdc5b926
salih <ensa0001@algonquinlive.com>
2021-05-27 19:47:53 -0400

    commit: Finished Phone Version of Lab 3

--- app/src/main/java/algonquin/cst2335/ensa0001/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/ensa0001/MainActivity.java
@@ -6,6 +6,9 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.view.accessibility.AccessibilityEvent;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -17,11 +20,33 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imgView;
+    Switch sw;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        imgView =findViewById(R.id.flagview);
+        sw = findViewById(R.id.spin_switch);
+
+        sw.setOnCheckedChangeListener((btn,isChecked) -> {
+            if (isChecked)
+            {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(1000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
+                imgView.startAnimation(rotate);
+            }
+            else {
+                imgView.clearAnimation();
+            }
+
+        });
 
 
     }
