6e95d2880649f2508dfee1a1e50273f79e82600a
salih <ensa0001@algonquinlive.com>
2021-06-08 23:25:59 -0400

    commit: Week_4 saving work

--- app/src/main/java/algonquin/cst2335/ensa0001/SecondActivity.java
+++ app/src/main/java/algonquin/cst2335/ensa0001/SecondActivity.java
@@ -3,8 +3,10 @@ package algonquin.cst2335.ensa0001;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
@@ -14,42 +16,51 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         ImageView  profileImage=findViewById(R.id.profileImage);
-        Intent fromNextPage = data;
-        if (requestCode==535){
+
+
+        if (requestCode==5432){
            if (resultCode==RESULT_OK){
                Bitmap thumbnail = data.getParcelableExtra("data");
                profileImage.setImageBitmap(thumbnail);
+               Intent fromNextPage = data;
+               try {
+                   FileOutputStream fOut = null;
+                   //Bitmap thumbnail = data.getParcelableExtra("data");
+                   fOut = openFileOutput( "Picture.png", Context.MODE_PRIVATE);
+                   thumbnail.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+                   fOut.flush();
+                   fOut.close();
+               } catch (IOException e) {
+                   e.printStackTrace();
 
+               }
            }
         }
+
+
+
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-
-
-
-
-
-
-
+        
         Intent fromPrevious = getIntent();
         String emailAddress = fromPrevious.getStringExtra("emailAddress");
-
         TextView topOfScreen = findViewById(R.id.textView);
-
         topOfScreen.setText("Welcome "+emailAddress);
-
-
-
 
         Button callButton = findViewById(R.id.callButton);
         callButton.setOnClickListener(  clk -> {
@@ -64,11 +75,13 @@ public class SecondActivity extends AppCompatActivity {
         Button changePictureButton = findViewById(R.id.changePictureButton);
         changePictureButton.setOnClickListener(  click -> {
             Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivityForResult( cameraIntent, 535);
+            startActivityForResult( cameraIntent, 5432);
 
-            //finish();
+            finish();
 
         } );
+
+
 
 
 
