664b08aaaf96d0fc28199a75b44e85cc555af941
salih <ensa0001@algonquinlive.com>
2021-06-13 00:41:56 -0400

    commit: Finished module 1

--- app/src/main/java/algonquin/cst2335/ensa0001/ChatRoom.java
+++ app/src/main/java/algonquin/cst2335/ensa0001/ChatRoom.java
@@ -2,18 +2,51 @@ package algonquin.cst2335.ensa0001;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Date;
 
 public class ChatRoom extends AppCompatActivity {
 
     @Override
-    public void onCreate( Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView( R.layout.chatlayout );
-         chatList = findViewById(R.id.myrecycler);
+        setContentView(R.layout.chatlayout);
+        RecyclerView chatList = findViewById(R.id.myrecycler);
+
+        chatList.setAdapter(new MyChatAdapter());
+    }
+
+    private class MyChatAdapter extends RecyclerView.Adapter {
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+    }
+
+
+    private class ChatMessage {
+        String message;
+        int sendORReceive;
+        Date timeSent;
 
     }
+
 }
